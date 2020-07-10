f3d5a6ee0ab18cf3f8a61ece4fd555f44b84687c
Matthew Cebenka <matthewt.cebenka@gmail.com>
2020-07-10 11:58:46 -0400

    oop1

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -16,4 +16,26 @@
 # class Whatever:
 #     pass
 #
+class Vehicle:
+    # base class
+    pass
+
+class GroundVehicle(Vehicle):
+    pass
+
+class Car(GroundVehicle):
+    pass
+
+class Motorcycle(GroundVehicle):
+    pass
+
+class FlightVehicle(Vehicle):
+    pass
+
+class Airplane(FlightVehicle):
+    pass
+
+class Starship(FlightVehicle):
+    pass
+
 # Put a comment noting which class is the base class
